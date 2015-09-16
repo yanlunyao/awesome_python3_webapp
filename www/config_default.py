8f9243651484e28750243c01e67997f16a1005c8
yanly <413899391@qq.com>
2015-09-16 21:12:43 +0800

    add initial resource

---
+++ www/config_default.py
@@ -0,0 +1,28 @@
+'''
+Created on: 2015年9月16日  
+@description: 
+@author: yanly
+@version   
+        V01-00
+'''
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yanly'
+
+'''
+Default configurations.
+'''
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,23 @@
+'''
+Created on: 2015年9月16日  
+@description: 
+@author: yanly
+@version   
+        V01-00
+'''
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yanly'
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
